7f7c76128d1e00e2678b301347df5cfa46cd2212
Vishnu <mvvishnu369@gmail.com>
2018-07-03 07:58:38 +0530

    july3-2

---
+++ js/strings.js
@@ -0,0 +1,41 @@
+var txt = "Hello World!";
+console.log(txt);
+
+
+var txt1 = "Hello World!";
+var len = txt1.length;
+console.log(len);
+
+
+var msg = "We are \"Vikings\"."; 
+console.log(msg);
+
+
+var str1 = "Hello ";
+var str2 = "World!";
+var str = str1 + str2;
+console.log(str);
+
+var txt2 = "Hello World";
+var i = txt2.indexOf("World")
+console.log(i);
+
+
+var strg1 = "Apple,Banana,Kiwi";
+console.log(strg1.slice(6));
+
+
+var txt3 = "Hello World";
+var res = txt3.replace("World","Universe");
+console.log(res);
+
+
+var txt4 = "Hello World";
+console.log(txt4.toUpperCase());
+console.log(txt4.toLowerCase());
+
+
+var str11 = "Hello ";
+var str22 = "World!";
+var str3 = str11.concat(str22); 
+console.log(str3);
